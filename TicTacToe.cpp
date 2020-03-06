29881f8f051f84fab6fae0ce62a92af04435cd9f
TheIrishBearcat24 <61879913+TheIrishBearcat24@users.noreply.github.com>
2020-03-06 11:13:31 -0500

    defined some variables

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <string>
 //going to need to include vector library and maybe stdlib?
 
 class TicTacToe {
+
+    char x = 'X';
+    char o = 'O';
+
+    //string ticTacToe = new string[3][3];
 
     int main() {
         return 0;
