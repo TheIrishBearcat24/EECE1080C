a26eba1e500d91e6cd0153963325735bc5b5ef9f
TheIrishBearcat24 <61879913+TheIrishBearcat24@users.noreply.github.com>
2020-03-06 12:40:46 -0500

    Was wondering why string wasn't working, forgot to include namespace std

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -2,12 +2,14 @@
 #include <string>
 //going to need to include vector library and maybe stdlib?
 
+using namespace std;
+
 class TicTacToe {
 
     char x = 'X';
     char o = 'O';
 
-    //string ticTacToe = new string[3][3];
+    string ticTacToe[3][3];
 
     int main() {
         return 0;
