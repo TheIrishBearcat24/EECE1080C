f312509d6510a0dba1bf13fdea3dc4c3b6389625
TheIrishBearcat24 <61879913+TheIrishBearcat24@users.noreply.github.com>
2020-03-09 16:49:52 -0400

    Tried re-adding TicTacToe

---
+++ TicTacToe.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+//going to need to include vector library and maybe stdlib?
+
+using namespace std;
+
+class TicTacToe {
+
+    char x = 'X';
+    char o = 'O';
+    char playAgain = 'Y';
+
+    char ticTacToe[3][3] = {{'-','-','-'},{'-','-','-'},{'-','-','-'}}; //put this inside the do while loop
+
+    //TO DO
+    //How would we implement a randomizational tool for the placement of the opponent's placement?
+    //How would we check for winning and losing?
+    //Going to need a do while loop to check for playing again
+
+    void hello() {
+        cout << "Do again? (Y/N): " << endl;
+        cin >> playAgain;
+        do {
+            cout << "Hi ";
+        }
+    
+        while (playAgain == 'Y');
+    }
+
+    int main() {
+        
+        hello();
+
+        return 0;
+    }
+};
