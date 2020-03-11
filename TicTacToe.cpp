ca719d4ada1ae8c87b4449381b52ca4c597097b8
TheIrishBearcat24 <61879913+TheIrishBearcat24@users.noreply.github.com>
2020-03-11 17:39:21 -0400

    Made improvements to TTT

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -20,6 +20,21 @@ using namespace std;
     //Need to make it that computer almost always wins
     //Machine learning aspect
 
+    void computeMoves() {
+        char firstMoveRow = rand() % 3;
+        char firstMoveCol = rand() % 3;
+        int userRowInput = 0;
+        int userColInput = 0;
+
+        cout << "Enter row index for user: " << endl;
+        cin >> userRowInput;
+        cout << "Enter column index for user: " << endl;
+        cin >> userColInput;
+
+        ticTacToe[firstMoveRow][firstMoveCol] = x;
+        ticTacToe[userRowInput][userColInput] = o;
+    }
+
     void ticTacToeBoard() {
         do {
             for (int i = 0; i < 3; i++) {
@@ -30,16 +45,14 @@ using namespace std;
                 cout << endl;
             }
 
+            computeMoves();
+
             cout << "Want to play again? (Y/N): ";
 
             cin >> playAgain;
         }
 
         while (playAgain == 'y' || playAgain == 'Y');
-    }
-
-    void computeMoves() {
-        char firstMove = rand() % 3;
     }
 
     int main() {
