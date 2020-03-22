25fa8d5dc245cac7addf8e91e8074a9d7eb928b5
TheIrishBearcat24 <61879913+TheIrishBearcat24@users.noreply.github.com>
2020-03-22 16:03:17 -0500

    Finished up tic tac toe code

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,63 +1,161 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
-#include <ctime>
-#include <cstdlib>
-//going to need to include vector library and maybe stdlib?
+#include <array>
 
 using namespace std;
 
-    char x = 'X';
-    char o = 'O';
-    char playAgain = 'Y';
+char x = 'X';
+char o = 'O';
+char playAgain = 'Y';
 
-    char ticTacToe[3][3] = {{'-','-','-'},{'-','-','-'},{'-','-','-'}}; //put this inside the do while loop
-    
-    //TO DO
-    //Make array of user inputs and computer inputs
-    //cout computer user and computer inputs
-    //After each input, the other player needs to make a move
-    //Computer always goes first
-    //Need to make it that computer almost always wins
-    //Machine learning aspect
+char ticTacToe[3][3] = {{'-','-','-'},{'-','-','-'},{'-','-','-'}}; //put this inside the do while loop
 
-    void computeMoves() {
-        char firstMoveRow = rand() % 3;
-        char firstMoveCol = rand() % 3;
-        int userRowInput = 0;
-        int userColInput = 0;
+//TO DO
+//Make array of user inputs and computer inputs
+//cout computer user and computer inputs
+//After each input, the other player needs to make a move
+//Computer always goes first
+//Need to make it that computer almost always wins
+//Machine learning aspect
 
-        cout << "Enter row index for user: " << endl;
-        cin >> userRowInput;
-        cout << "Enter column index for user: " << endl;
-        cin >> userColInput;
+void ticTacToeBoard() {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << ticTacToe[i][j] << " ";
+        }
 
-        ticTacToe[firstMoveRow][firstMoveCol] = x;
-        ticTacToe[userRowInput][userColInput] = o;
+        cout << endl;
+    }
+}
+
+void computerChoice() {
+    bool checker = true;
+    int i = 0;
+
+    cout << "Board after computer move: " << endl;
+    cout << endl;
+
+    while (checker == true) {
+        if (ticTacToe[i / 4][int (i % 4)] == '-') { // uses an iterator to check if the next space is an empty slot
+            ticTacToe[i / 4][int (i % 4)] = x;
+            checker = false;
+        }
+
+        i += 1;
     }
 
-    void ticTacToeBoard() {
-        do {
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    cout << ticTacToe[i][j] << " ";
-                }
+    ticTacToeBoard();
+}
 
-                cout << endl;
-            }
+void userChoice() {
+    int row, col;
 
-            computeMoves();
+    again: // acts as a do-while loop
+    cout << "Choose row for user: (0, 1, 2): " << endl;
+    cin >> row;
+    cout << "Choose column for user (0, 1, 2): " << endl;
+    cin >> col;
+    cout << endl;
 
-            cout << "Want to play again? (Y/N): ";
+    if (ticTacToe[col][row] == x || ticTacToe[col][row] == o) {
+        cout << "Slot already taken. Please try again." << endl;
+        goto again;
+    }
 
+    else {
+        ticTacToe[row][col] = o;
+        cout << "Board after user input: " << endl;
+        ticTacToeBoard();
+    }
+}
+
+bool checkForWin() {
+    bool hasWon = false;
+    int userWonFlag = 0;
+    int playAgain;
+
+    for (int i = 0; i < 3; i++) {
+        if (ticTacToe[0][i] == o && ticTacToe[1][i] == o && ticTacToe[2][i] == o) {
+            hasWon = true;
+            userWonFlag = 1;
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        if (ticTacToe[i][0] == o && ticTacToe[i][1] == o && ticTacToe[i][2] == o) {
+            hasWon = true;
+            userWonFlag = 1;
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        if (ticTacToe[0][i] == x && ticTacToe[1][i] == x && ticTacToe[2][i] == x) {
+            hasWon = true;
+            userWonFlag = -1;
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        if (ticTacToe[i][0] == x && ticTacToe[i][1] == x && ticTacToe[i][2] == x) {
+            hasWon = true;
+            userWonFlag = -1;
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        if (ticTacToe[0][0] == x && ticTacToe[1][1] == x && ticTacToe[2][2] == x) {
+            hasWon = true;
+            userWonFlag = -1;
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        if (ticTacToe[2][0] == x && ticTacToe[1][1] == x && ticTacToe[2][0] == x) {
+            hasWon = true;
+            userWonFlag = -1;
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        if (ticTacToe[0][0] == o && ticTacToe[1][1] == o && ticTacToe[2][2] == o) {
+            hasWon = true;
+            userWonFlag = 1;
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        if (ticTacToe[2][0] == o && ticTacToe[1][1] == o && ticTacToe[2][0] == o) {
+            hasWon = true;
+            userWonFlag = 1;
+        }
+    }
+
+    if (hasWon == true) {
+        if (userWonFlag == 1) {
+            cout << "You won! Play again? " << endl;
             cin >> playAgain;
         }
 
-        while (playAgain == 'y' || playAgain == 'Y');
+        if (userWonFlag == -1) {
+            cout << "Computer won! Play again? " << endl;
+            cin >> playAgain;
+        }
     }
 
-    int main() {
-        
-        ticTacToeBoard();
+    return true;
+}
 
-        return 0;
+int main() {
+    
+    bool runGame = true;
+
+    while (runGame == true) {
+        computerChoice();
+        runGame = checkForWin();
+        userChoice();
+        runGame = checkForWin();
     }
+
+    return 0;
+}
